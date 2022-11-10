78d652c266d8edcfef32bb666ca05fb9350d12ee
BT_4N2_31 <tdukdak9287@bths.edu>
2022-11-10 12:05:00 -0500

    I added my constructor and completed two methods getElement and getZodiac

--- src/HoroscopeTeller.java
+++ src/HoroscopeTeller.java
@@ -1,2 +1,62 @@
 public class HoroscopeTeller {
+    private int birthYear;
+    private int birthMonth;
+    private int birthDay;
+    private String zodiac;
+    private String element;
+
+    public HoroscopeTeller(int month, int day, int year){
+        month = birthMonth;
+        day = birthDay;
+        year = birthYear;
+    }
+    public String getZodiac(int month, int day){
+        if((month == 12 && day >= 22 && day <= 31) || (month ==  1 && day >= 1 && day <= 19))
+            zodiac = "Capricorn";
+        else if ((month ==  1 && day >= 20 && day <= 31) || (month ==  2 && day >= 1 && day <= 17))
+            zodiac = "Aquarius";
+        else if ((month ==  2 && day >= 18 && day <= 29) || (month ==  3 && day >= 1 && day <= 19))
+            zodiac = "Pisces";
+        else if ((month ==  3 && day >= 20 && day <= 31) || (month ==  4 && day >= 1 && day <= 19))
+            zodiac = "Aries";
+        else if ((month ==  4 && day >= 20 && day <= 30) || (month ==  5 && day >= 1 && day <= 20))
+            zodiac = "Taurus";
+        else if ((month ==  5 && day >= 21 && day <= 31) || (month ==  6 && day >= 1 && day <= 20))
+            zodiac = "Gemini";
+        else if ((month ==  6 && day >= 21 && day <= 30) || (month ==  7 && day >= 1 && day <= 22))
+            zodiac = "Cancer";
+        else if ((month ==  7 && day >= 23 && day <= 31) || (month ==  8 && day >= 1 && day <= 22))
+            zodiac = "Leo";
+        else if ((month ==  8 && day >= 23 && day <= 31) || (month ==  9 && day >= 1 && day <= 22))
+            zodiac = "Virgo";
+        else if ((month ==  9 && day >= 23 && day <= 30) || (month == 10 && day >= 1 && day <= 22))
+            zodiac = "Libra";
+        else if ((month == 10 && day >= 23 && day <= 31) || (month == 11 && day >= 1 && day <= 21))
+            zodiac = "Scorpio";
+        else if ((month == 11 && day >= 22 && day <= 30) || (month == 12 && day >= 1 && day <= 21))
+            zodiac = "Sagittarius";
+        else
+            System.out.println("unidentifiable");
+        return zodiac;
+    }
+    public String getElement(String zodiacElement){
+        zodiacElement = zodiac;
+        if(zodiac.equals("Aries") || zodiac.equals("Leo") || zodiac.equals("Sagittarius")){
+            element = "Fire sign";
+        }
+        else if(zodiac.equals("Taurus") || zodiac.equals("Virgo") || zodiac.equals("Capricorn")){
+            element = "Earth sign";
+        }
+        else if(zodiac.equals("Gemini") || zodiac.equals("Libra") || zodiac.equals("Aquarius")){
+            element = "Air sign";
+        }
+        else{
+            element = "Water sign";
+        }
+        return element;
+    }
+    public String getFortune(int randomNumber){
+        return "fortune";
+    }
+
 }
--- src/designDocument.java
+++ src/designDocument.java
@@ -1,2 +1,36 @@
-public class designDocument {
-}
+/*
+HoroscopeTeller class
+
+Instance variables:
+Private int birthYear - user inputted birth year
+Private int birthMonth - user inputted birth month
+Private int birthDay - user inputted birth day
+Private String zodiac - this will be found out later based on the user data
+
+Public Horoscope(int birthMonth,  int birthDay, int birthYear)
+This constructor will contain all the birth variables which will be used later to determine horoscope
+
+Public String getElement(birthMonth)
+Will return the zodiac element
+
+Public String getZodiac(birthMonth, birthDay)
+Will return the zodiac using the dates inputted and set it equal to the instance variable
+
+Public String getFortune(int randomNumber)
+Will return a random future depending on the birthYear date
+
+Public int yearsLeft()
+Predicts a random lifespan left depending on birthYear
+
+HoroscopeTellerRunner class
+Ask for the users birth info
+Convert the input and attach it to the variables
+Attach the variables to an object
+Call the getZodiac method to deduce the horoscope
+Call the getElement method then to deduce the horoscope
+Ask if they wish to hear their fortune
+Ask for a random number from 1-10
+Give a random fortune based off the number
+Then display lifespan left
+
+ */
